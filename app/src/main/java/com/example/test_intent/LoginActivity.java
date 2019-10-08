b435b16dfbd250f7edf9dee5120b78f0db6dfb73
vinhnghi55 <vinhnghi789@gmail.com>
2019-10-08 13:51:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test_intent/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.test_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acrivity_login);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
